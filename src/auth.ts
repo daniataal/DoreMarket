e835f0e6403d2533ea0430f313dcbb275dd33749
daniataal <daneyataal@gmail.com>
2026-02-13 17:29:39 +0200

    auth

--- src/auth.ts
+++ src/auth.ts
@@ -30,7 +30,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                     if (!user) return null;
 
                     const passwordsMatch = await bcrypt.compare(password, user.password);
-                    if (passwordsMatch) return user;
+                    if (passwordsMatch) {
+                        return {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email,
+                            role: user.role,
+                        };
+                    }
                 }
 
                 console.log('Invalid credentials');
